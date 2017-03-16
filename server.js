dd7375aa2435e42bd1ee4ac581bb0c1a908abf29
Khang Hoang <hoangtrieukhang@gmail.com>
2017-03-16 21:58:50 +0700

    fix/add CORS

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.use(compression());
 app.use(expressValidator());
 app.use(logger('dev'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(session({
   resave: true,
   saveUninitialized: true,
